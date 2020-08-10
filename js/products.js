c4a255f3afd548b5feaf86a957e0b2e2823c0259
stefanisilvagarrido <teefi_@hotmail.com>
2020-08-09 22:35:16 -0300

    Agregar productos a JS
    Creamos el Fetch y agregamos la lista de productos.
    También hicimos modificaciones.

--- js/products.js
+++ js/products.js
@@ -2,5 +2,37 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    fetch(PRODUCTS_URL)
+
+   
+        .then(res => res.json())
+        .then(res => {
+            var listaProductos= document.getElementById("cat-list-container")
+
+            for(var producto of res){
+              listaProductos.innerHTML+=  `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ producto.name +`</h4>
+                            <small class="text-muted">` + producto.currency + ` ` + producto.cost + `</small>
+                        </div>
+                        <p class="mb-1">` + producto.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+            }
+        })
+
+
+
+
+
+
 
 });
