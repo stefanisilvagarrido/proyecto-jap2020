eddfa4c149844139b4853530a0344709cdcb718c
stefanisilvagarrido <teefi_@hotmail.com>
2020-08-15 21:51:48 -0300

    Se creó código JS.
    Código JS para funcionalidades del login.

--- js/login.js
+++ js/login.js
@@ -2,5 +2,11 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+var mail= document.getElementById("inputEmail") //Buscar la etiqueta donde igresamos el mail, en el html.
+var ingresarEmail= document.getElementById("buttonEmail")//Buscar la etiqueta del botón para ingresar al index.html
+ingresarEmail.addEventListener("click", ()=>{ //Esta función lo que hace es: cuando damos click en el botón, guardamos nuestro email, en sessionStorage.
+    window.sessionStorage.setItem("mail", mail.value)
+})
+  
 
 });
